e5e727074f1bd13091f523ba7390faf3f21ce298
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2021-03-24 16:27:09 -0300

    added tflite

---
+++ lib/models/examples/examples.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:plant_diagnosis/helpers/types.dart';
+
+class Examples {
+  final String description;
+  final String location;
+  final Types type;
+
+  Examples(
+      {@required this.description,
+      @required this.location,
+      @required this.type});
+}
+
+final listOfExamples = [
+  Examples(
+    description: 'Cats and Dogs',
+    location: 'cats_and_dogs',
+    type: Types.CATS_AND_DOGS,
+  ),
+  Examples(
+    description: 'Strawberries',
+    location: 'strawberries',
+    type: Types.STRAWBERRIES,
+  ),
+  Examples(
+    description: 'EasyAI',
+    location: 'easyai',
+    type: Types.EASY_AI,
+  )
+];
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:plant_diagnosis/models/examples/examples.dart';
+import 'package:plant_diagnosis/pages/classified_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 100),
+          Text(
+            'Choose what you want classified!',
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          ListView.builder(
+            itemCount: listOfExamples.length,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(listOfExamples[index].description),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ClassifiedPage(
+                        example: listOfExamples[index],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
